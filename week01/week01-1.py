e5b1b3943dd5f45c366ecea38086b0a46d267500
Fiona <13750686@me.mcu.edu.tw>
2026-04-12 14:23:40 +0800

    week01

---
+++ week01/week01-1.py
@@ -0,0 +1,11 @@
+# week01-1.py
+#1404
+class Solution:
+    def numSteps(self, s: str) -> int:
+        ans = 0
+        n = int(s,2)
+        while n>1:
+            if n%2==0: n//=2
+            else: n+=1
+            ans += 1
+        return ans
