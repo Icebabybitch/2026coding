99738ebbaee609ab1cc4cc557df4c990c455c1be
Fiona <13750686@me.mcu.edu.tw>
2026-04-12 17:22:59 +0800

    week02

---
+++ week02/week02-5.py
@@ -0,0 +1,16 @@
+# week02-5.py
+# 1679
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 0
+        i, j = 0, len(nums)-1
+        while i<j:
+            if nums[i] + nums[j] == k:
+                ans += 1
+                i, j = i+1, j-1
+            if nums[i] + nums[j] < k:
+                i += 1
+            if nums[i] + nums[j] > k:
+                j -= 1
+        return ans
