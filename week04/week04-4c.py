40660a10aa7749bb01a09355c9939a81580f45f4
Fiona <13750686@me.mcu.edu.tw>
2026-04-16 01:57:08 +0800

    week04

---
+++ week04/week04-4c.py
@@ -0,0 +1,9 @@
+# week04-4c.py(­«¼gweek04-4b.py)
+# 3866
+class Solution:
+    def firstUniqueEven(self, nums: list[int]) -> int:
+        H = Counter(nums)
+        for nn in nums:
+            if nn%2 == 0 and H[nn] ==1:
+                return nn
+        return -1
